a375d4bd9b97d4cfd19045d51f5247c8f23ac8c0
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-06-03 15:08:56 +0200

    fix: displays todos for post /todos

--- src/routes/todos/todos.query.js
+++ src/routes/todos/todos.query.js
@@ -3,19 +3,42 @@ const { user } = require("../user/user");
 
 
 exports.pick_todo = function(user_id, callback) {
-    db.execute('SELECT title, description, due_time, user_id, status  FROM todo WHERE user_id = ?', [user_id], (err, results) => {
+    db.execute('SELECT id, title, description, created_at, due_time, user_id, status FROM todo WHERE id = ?', [user_id], (err, results) => {
         if (err)
             return callback(err, null);
         if (results.length > 0) {
             const todo_info = results[0];
             const formatted = new Date(todo_info.due_time).toISOString().slice(0, 19).replace('T', ' ');
             todo_info.due_time = formatted;
+            const form = new Date(todo_info.created_at).toISOString().slice(0, 19).replace('T', ' ');
+            todo_info.created_at = form;
             callback(null, todo_info);
         } else {
             callback(null, null);
         }
     });
 }
+
+module.exports.create_todo = function(title, description, due_time, user_id, status, callback) {
+    db.execute( 'INSERT INTO todo (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)',
+        [title, description, due_time, user_id, status],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            }
+            if (result.affectedRows > 0) {
+                exports.pick_todo(result.insertId, (err, todo_info) => {
+                    if (err)
+                        return callback(err);
+                    return callback(null, true, todo_info);
+                });
+            } else {
+                return callback(null, false, null);
+            }
+        }
+    );
+};
 
 module.exports.pick_all_todos = function(callback) {
     db.execute('SELECT id, title, description,  created_at, due_time, user_id, status FROM todo', (err, results) => {
@@ -59,26 +82,6 @@ module.exports.todo_id = function(id, callback) {
     });
 }
 
-module.exports.create_todo = function(title, description, due_time, user_id, status, callback) {
-    db.execute( 'INSERT INTO todo (title, description, due_time, user_id, status) VALUES (?, ?, ?, ?, ?)',
-        [title, description, due_time, user_id, status],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-                return callback(err, null);
-            }
-            if (result.affectedRows > 0) {
-                exports.pick_todo(user_id, (err, todo_info) => {
-                    if (err)
-                        return callback(err);
-                    return callback(null, true, todo_info);
-                });
-            } else {
-                return callback(null, false, null);
-            }
-        }
-    );
-};
 
 exports.delete_todo_db = function(id, callback) {
     db.execute('DELETE FROM todo WHERE id = ?', [id], (err, result) => {
