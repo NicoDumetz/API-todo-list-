a501ef10199ca2bd4d1f4fe0d5408fa03e8955ac
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-06-03 15:08:24 +0200

    fix: console log .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,28 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const HOST = process.env.MYSQL_HOST;
 const USER = process.env.MYSQL_USER;
-const PASSWORD = process.env.MYSQL_ROOT_PASSWORD;
 const DATABASE = process.env.MYSQL_DATABASE;
-
+const PASSWORD = process.env.MYSQL_ROOT_PASSWORD;
 
 const connection = mysql.createConnection({
-    host: HOST,
-    user: USER,
-    password: PASSWORD,
-    database: DATABASE
+  host: HOST,
+  user: USER,
+  database: DATABASE,
+  password: PASSWORD,
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error with the connection of the database small bytes.' + err);
-        return;
-    }
-    console.log('You are conected to the database small bytes.');
+  console.log(HOST, USER, PASSWORD, DATABASE);
+  if (err) {
+    console.error(
+      "Error with the connection of the database small bytes." + err
+    );
+    return;
+  }
+  console.log("You are conected to the database small bytes.");
 });
-module.exports = connection;
+module.exports = { connection };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-require('dotenv').config();
-var bcrypt = require('bcryptjs');
-const connection = require('./config/db');
-const mysql = require('mysql2');
-const express = require('express');
-const auth = require('./routes/auth/auth');
-const user = require('./routes/user/user');
-const todos = require('./routes/todos/todos');
-
+require("dotenv").config();
+var bcrypt = require("bcryptjs");
+const { connection: db } = require("./config/db");
+const mysql = require("mysql2");
+const express = require("express");
+const auth = require("./routes/auth/auth");
+const user = require("./routes/user/user");
+const todos = require("./routes/todos/todos");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.raw());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 auth.register(app, bcrypt);
 auth.login(app, bcrypt);
@@ -26,5 +25,5 @@ todos.todo_delete(app);
 todos.todo_query(app);
 
 app.listen(PORT, () => {
-    console.log(`Serveur open on the port: ${PORT}`);
+  console.log(`Serveur open on the port: ${PORT}`);
 });
