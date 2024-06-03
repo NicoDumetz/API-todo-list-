5322eb41605202f1f10faffd56d307d82e004ed3
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-06-03 15:08:42 +0200

    fix: epytodo sql with good type

--- epytodo.sql
+++ epytodo.sql
@@ -2,23 +2,21 @@ CREATE DATABASE IF NOT EXISTS epytodo;
 USE epytodo;
 
 CREATE TABLE IF NOT EXISTS user (
-    id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    firstname VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email varchar(255) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  firstname varchar(255) NOT NULL,
+  name varchar(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS todo (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    due_time DATETIME,
-    status ENUM('not started', 'todo', 'in progress', 'done') DEFAULT 'not started',
-    user_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES user(id)
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  description text NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  due_time datetime NOT NULL,
+  user_id int NOT NULL,
+  status ENUM('not started', 'todo', 'in progress', 'done') NOT NULL DEFAULT 'not started',
+  FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
