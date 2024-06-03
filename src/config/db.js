3ded2ff92d4e6386bda6a8b986e3f66b0c6a13f7
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-06-03 15:08:22 +0200

    fix: add dotenv on db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 const HOST = process.env.MYSQL_HOST;
 const USER = process.env.MYSQL_USER;
 const PASSWORD = process.env.MYSQL_ROOT_PASSWORD;
